ac34c90724cd49fa9d733e8f0237f581e8adff31
BrennoTreggi <brennotreggi2@hotmail.com>
2026-04-16 23:31:37 -0300

    imagens

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-function trocarPagina(pagina){
-  document.querySelectorAll("section").forEach(sec=>{
-    sec.classList.remove("active");
+const slides = document.querySelectorAll('.slide');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const close = document.querySelector('.close');
+
+slides.forEach(slide => {
+  slide.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImg.src = slide.src;
   });
+});
 
-  document.getElementById(pagina).classList.add("active");
-}
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
-function calcular(){
-  let preco = document.getElementById("servico").value;
-  let qtd = document.getElementById("qtd").value;
-
-  let total = preco * qtd;
-
-  document.getElementById("total").innerText = "Total: R$" + total;
-}
-function trocarImagem(botao, direcao){
-  const card = botao.parentElement;
-  const imagens = card.querySelectorAll(".carrossel img");
-
-  let index = 0;
-
-  imagens.forEach((img, i) => {
-    if(img.classList.contains("ativa")){
-      index = i;
-      img.classList.remove("ativa");
-    }
-  });
-
-  index += direcao;
-
-  if(index < 0) index = imagens.length - 1;
-  if(index >= imagens.length) index = 0;
-
-  imagens[index].classList.add("ativa");
-}
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
