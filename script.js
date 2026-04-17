30788baa971a8d514693c87045985fb300301159
BrennoTreggi <brennotreggi2@hotmail.com>
2026-04-16 22:43:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ function calcular(){
 
   document.getElementById("total").innerText = "Total: R$" + total;
 }
+function trocarImagem(botao, direcao){
+  const card = botao.parentElement;
+  const imagens = card.querySelectorAll(".carrossel img");
+
+  let index = 0;
+
+  imagens.forEach((img, i) => {
+    if(img.classList.contains("ativa")){
+      index = i;
+      img.classList.remove("ativa");
+    }
+  });
+
+  index += direcao;
+
+  if(index < 0) index = imagens.length - 1;
+  if(index >= imagens.length) index = 0;
+
+  imagens[index].classList.add("ativa");
+}
