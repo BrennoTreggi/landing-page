2b2894a8fff5b2ee45a18aa3dc16d81e0489bfc0
BrennoTreggi <brennotreggi2@hotmail.com>
2026-04-26 04:06:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post('/criar-pagamento', async (req, res) => {
         }
       ],
       payer: {
-        email: 'test@testuser.com'
+        email: 'test_user_3795585682180619933@testuser.com'
       },
       payment_methods: {
         excluded_payment_methods: [],
