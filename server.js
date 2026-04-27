97da5314faf3dd1697fd544fb7b25e5ed678a7ae
BrennoTreggi <brennotreggi2@hotmail.com>
2026-04-27 03:36:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.post('/process_payment', async (req, res) => {
  
    
    
-   //  let valorOriginal = parseFloat(transactionAmount);
+     let valorOriginal = parseFloat(transactionAmount);
    //  let valorFinal = valorOriginal;
     // let parcelasPermitidas = 1;
    //  let parcelasSolicitadas = parseInt(installments) || 1;
